a7c657358721bda25bb81b1e8fb39f97dd23566b
yahya nashar <cenations.people@live.com>
2023-08-31 00:01:00 +0300

    JS-Basics-2 Step1

---
+++ javascript-basics-02/step 1/index.js
@@ -0,0 +1,3 @@
+const image = document.querySelector('#image1');
+image.addEventListener('mouseover', function(){image.src="images/image1_2.jpg"});
+image.addEventListener('mouseout', function(){image.src="images/image1.jpg"});
